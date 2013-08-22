a8445750dcf527d1cedd1fab66c7340bca5200aa
Juda Kaleta <juda.kaleta@gmail.com>
2013-08-22 15:44:11 +0200

    Embed video settings file

---
+++ embed_video/settings.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+
+EMBED_VIDEO_BACKENDS = getattr(settings, 'EMBED_VIDEO_BACKENDS', (
+    'embed_video.backends.YoutubeBackend',
+    'embed_video.backends.VimeoBackend',
+    'embed_video.backends.SoundCloudBackend',
+))
