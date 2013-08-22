6df2442914088653108bd74ee82e132f1da0a9b3
Juda Kaleta <juda.kaleta@gmail.com>
2013-08-22 12:03:42 +0200

    Ready for Python 3

--- embed_video/tests/tests.py
+++ embed_video/tests/tests.py
@@ -2,6 +2,6 @@ import os
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'embed_video.tests.django_settings'
 
-from tests_fields import EmbedVideoFieldTestCase, EmbedVideoFormFieldTestCase
-from tests_backend import EmbedVideoTestCase
-from tests_tags import EmbedVideoNodeTestCase
+from .tests_fields import EmbedVideoFieldTestCase, EmbedVideoFormFieldTestCase
+from .tests_backend import EmbedVideoTestCase
+from .tests_tags import EmbedVideoNodeTestCase
