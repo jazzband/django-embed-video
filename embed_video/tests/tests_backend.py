be7a53296b14005d08ac8139484a2ebe0303c372
Juda Kaleta <juda.kaleta@gmail.com>
2013-10-04 09:45:21 +0200

    Split backends tests into multiple test cases

--- embed_video/tests/tests_backend.py
+++ embed_video/tests/tests_backend.py
@@ -5,7 +5,7 @@ from ..backends import detect_backend, YoutubeBackend, VimeoBackend, \
         VideoDoesntExistException, UnknownIdException
 
 
-class BackendsTestCase(TestCase):
+class VideoBackendTestCase(TestCase):
     unknown_backend_urls = (
         'http://myurl.com/?video=http://www.youtube.com/watch?v=jsrRJyHBvzw',
         'http://myurl.com/?video=www.youtube.com/watch?v=jsrRJyHBvzw',
@@ -13,6 +13,12 @@ class BackendsTestCase(TestCase):
         'http://vimeo.com.myurl.com/72304002',
     )
 
+    def test_detect_bad_urls(self):
+        for url in self.unknown_backend_urls:
+            self.assertRaises(UnknownBackendException, detect_backend, url)
+
+
+class YoutubeBackendTestCase(TestCase):
     youtube_urls = (
         ('http://youtu.be/jsrRJyHBvzw', 'jsrRJyHBvzw'),
         ('http://youtu.be/n17B_uFF4cA', 'n17B_uFF4cA'),
@@ -32,6 +38,24 @@ class BackendsTestCase(TestCase):
         ('https://www.youtube.com/watch?feature=player_embedded&v=2NpZbaAIXag', '2NpZbaAIXag'),
     )
 
+    def test_detect_youtube(self):
+        for url in self.youtube_urls:
+            backend = detect_backend(url[0])
+            self.assertIsInstance(backend, YoutubeBackend)
+
+    def test_code_youtube(self):
+        for url in self.youtube_urls:
+            backend = YoutubeBackend(url[0])
+            code = backend.get_code()
+            self.assertEqual(code, url[1])
+
+    def test_youtube_keyerror(self):
+        """ Test for issue #7 """
+        self.assertRaises(UnknownIdException, YoutubeBackend,
+                          'http://youtube.com/watch?id=5')
+
+
+class VimeoBackendTestCase(TestCase):
     vimeo_urls = (
         ('http://vimeo.com/72304002', '72304002'),
         ('https://vimeo.com/72304002', '72304002'),
@@ -41,51 +65,14 @@ class BackendsTestCase(TestCase):
         ('https://player.vimeo.com/video/72304002', '72304002'),
     )
 
-    soundcloud_urls = (
-        ('https://soundcloud.com/glassnote/mumford-sons-i-will-wait', '67129237'),
-        ('https://soundcloud.com/matej-roman/jaromir-nohavica-karel-plihal-mikymauz', '7834701'),
-        ('https://soundcloud.com/beny97/sets/jaromir-nohavica-prazska', '960591'),
-        ('https://soundcloud.com/corbel-keep/norah-jones-come-away-with', '22485933'),
-    )
-
-    def setUp(self):
-        from django.conf import settings as django_settings
-        self.django_settings = django_settings
-
-    def test_detect_bad_urls(self):
-        for url in self.unknown_backend_urls:
-            self.assertRaises(UnknownBackendException, detect_backend, url)
-
-    def test_detect_youtube(self):
-        for url in self.youtube_urls:
-            backend = detect_backend(url[0])
-            self.assertIsInstance(backend, YoutubeBackend)
-
     def test_detect_vimeo(self):
         for url in self.vimeo_urls:
             backend = detect_backend(url[0])
             self.assertIsInstance(backend, VimeoBackend)
 
-    def test_detect_soundcloud(self):
-        for url in self.soundcloud_urls:
-            backend = detect_backend(url[0])
-            self.assertIsInstance(backend, SoundCloudBackend)
-
-    def test_code_youtube(self):
-        for url in self.youtube_urls:
-            backend = YoutubeBackend(url[0])
-            code = backend.get_code()
-            self.assertEqual(code, url[1])
-
     def test_code_vimeo(self):
         for url in self.vimeo_urls:
             backend = VimeoBackend(url[0])
-            code = backend.get_code()
-            self.assertEqual(code, url[1])
-
-    def test_code_soundcloud(self):
-        for url in self.soundcloud_urls:
-            backend = SoundCloudBackend(url[0])
             code = backend.get_code()
             self.assertEqual(code, url[1])
 
@@ -94,8 +81,22 @@ class BackendsTestCase(TestCase):
             backend = VimeoBackend('http://vimeo.com/123')
             backend.get_info()
 
-    def test_youtube_keyerror(self):
-        """ Test for issue #7 """
-        self.assertRaises(UnknownIdException, YoutubeBackend,
-                          'http://youtube.com/watch?id=5')
 
+class SoundCloudBackendTestCase(TestCase):
+    soundcloud_urls = (
+        ('https://soundcloud.com/glassnote/mumford-sons-i-will-wait', '67129237'),
+        ('https://soundcloud.com/matej-roman/jaromir-nohavica-karel-plihal-mikymauz', '7834701'),
+        ('https://soundcloud.com/beny97/sets/jaromir-nohavica-prazska', '960591'),
+        ('https://soundcloud.com/corbel-keep/norah-jones-come-away-with', '22485933'),
+    )
+
+    def test_detect_soundcloud(self):
+        for url in self.soundcloud_urls:
+            backend = detect_backend(url[0])
+            self.assertIsInstance(backend, SoundCloudBackend)
+
+    def test_code_soundcloud(self):
+        for url in self.soundcloud_urls:
+            backend = SoundCloudBackend(url[0])
+            code = backend.get_code()
+            self.assertEqual(code, url[1])
