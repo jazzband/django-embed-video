9df71d2d5f94bcaa3e97d52b5dd54e050af49adb
Juda Kaleta <juda.kaleta@gmail.com>
2013-10-04 09:37:57 +0200

    Apply admin mixin in example project

--- example_project/posts/admin.py
+++ example_project/posts/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
+from embed_video.admin import AdminVideoMixin
+
 from .models import Post
 
 
-admin.site.register(Post)
+class PostAdmin(AdminVideoMixin, admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Post, PostAdmin)
