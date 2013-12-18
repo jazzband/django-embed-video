6ceb636c5726f6a00d7f71e84eaab759f0687a26
Juda Kaleta <juda.kaleta@gmail.com>
2013-12-18 09:11:47 +0100

    Fix urls in example project

--- example_project/posts/urls.py
+++ example_project/posts/urls.py
@@ -4,5 +4,5 @@ from .views import PostListView, PostDetailView
 
 urlpatterns = patterns('',
     url(r'(?P<pk>\d+)/$', PostDetailView.as_view(), name='detail'),
-    url(r'/$', PostListView.as_view(), name='list'),
+    url(r'$', PostListView.as_view(), name='list'),
 )
