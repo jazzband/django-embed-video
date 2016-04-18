7b6df52fc801e68a4bc3c6e1ab9e8585c6adb8ff
Thijs Triemstra <info@collab.nl>
2016-04-18 21:46:45 +0200

    add html5 test urls

--- embed_video/tests/backends/tests_html5backend.py
+++ embed_video/tests/backends/tests_html5backend.py
@@ -1,25 +1,28 @@
 from unittest import TestCase
 
 from . import BackendTestMixin
-from embed_video.backends import (Html5VideoBackend, UnknownIdException,
-    Html5AudioBackend)
+from embed_video import backends
+
+
+class HTML5BackendTestCase(TestCase):
+    def test_url(self):
+        url = 'https://collab-project.github.io/videojs-wavesurfer/examples/media/heres_johnny.wav'
+        backend = backends.detect_backend(url)
+        self.assertEqual(backend.get_url(), url)
 
 
 class Html5VideoBackendTestCase(BackendTestMixin, TestCase):
     urls = (
-        ('http://example.com/test.mp4', None),
-        ('https://example.com/test/foo123.mov', None),
-        ('http://example.com/#/test.webm', None),
+        ('http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4', None),
+        ('https://collab-project.github.io/videojs-wavesurfer/examples/media/example.mp4', None),
     )
 
-    instance = Html5VideoBackend
+    instance = backends.Html5VideoBackend
 
 
 class Html5AudioBackendTestCase(BackendTestMixin, TestCase):
     urls = (
-        ('http://example.com/test.mp3', None),
-        ('https://example.com/test/foo123.wav', None),
-        ('http://example.com/#/test.webm', None),
+        ('https://collab-project.github.io/videojs-wavesurfer/examples/media/heres_johnny.wav', None),
     )
 
-    instance = Html5AudioBackend
+    instance = backends.Html5AudioBackend
