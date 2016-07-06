15813fa7cbf0242b4a2925f7a15ccb2155aa0d64
Brady Moe <bmoe872@gmail.com>
2016-07-06 16:21:57 -0500

    adds wistia backened class and some tests
    adds wistia video support for embeds

--- embed_video/settings.py
+++ embed_video/settings.py
@@ -4,6 +4,7 @@ from django.conf import settings
 EMBED_VIDEO_BACKENDS = getattr(settings, 'EMBED_VIDEO_BACKENDS', (
     'embed_video.backends.YoutubeBackend',
     'embed_video.backends.VimeoBackend',
+    'embed_video.backends.WistiaBackend',
     'embed_video.backends.SoundCloudBackend',
 ))
 """ :type: tuple[str] """
