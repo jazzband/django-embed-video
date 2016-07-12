90c6bf44c682e1f5a66f388ffdacb64e20e35d52
Brady Moe <bmoe872@gmail.com>
2016-07-12 12:15:36 -0500

    removes client tests, and adds another test url

--- embed_video/tests/backends/tests_wistia.py
+++ embed_video/tests/backends/tests_wistia.py
@@ -10,19 +10,20 @@ class WistiaBackendTestCase(BackendTestMixin, TestCase):
 
     urls = (
         ('https://support.wistia.com/medias/26sk4lmiix', '26sk4lmiix'),  # This comes from the wistia docs
+        ('https://home.wistia.com/medias/342jss6yh5', '342jss6yh5'),
     )
 
     instance = WistiaBackend
 
     def test_wistia_get_info_exception(self):
         with self.assertRaises(VideoDoesntExistException):
-            backend = WistiaBackend('http://gxsc.wistia.com/123')
+            backend = WistiaBackend('http://support.wistia.com/123')
             backend.get_info()
 
     def test_get_thumbnail_url(self):
-        backend = WistiaBackend('https://gxsc.wistia.com/medias/2y1o2i2vx6')
+        backend = WistiaBackend('https://home.wistia.com/medias/342jss6yh5')
         self.assertEqual(backend.get_thumbnail_url(),
-                         'https://embed-ssl.wistia.com/deliveries/14fb3042a6df04d57a6ed1a659f737d30b6d9e5c.jpg?image_crop_resized=1920x1080')
+                         'https://embed-ssl.wistia.com/deliveries/7fc8174a908ad1d349196405671e0fbdaa1e84eb.jpg?image_crop_resized=960x540')
 
     @patch('embed_video.backends.EMBED_VIDEO_TIMEOUT', 0.000001)
     def test_timeout_in_get_info(self):
