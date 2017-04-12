f32128f7c5cefcf9d64f895dc97aaf861b83ab98
Alexey Kotlyarov <a@koterpillar.com>
2017-04-13 07:49:35 +1000

    Update instructions for adding request context processor

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -25,6 +25,7 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
+                'django.template.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
