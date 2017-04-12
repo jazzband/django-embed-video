daba056c8a1e53a6ac557f33b6bde307b981dc39
Alexey Kotlyarov <a@koterpillar.com>
2017-04-12 21:26:10 +1000

    Remove usage of urlpatterns

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = staticfiles_urlpatterns() \
-    + patterns('',
+    + [
         url(r'^admin/', include(admin.site.urls)),
         url(r'^', include('posts.urls', namespace='posts')),
-    )
+    ]
--- example_project/posts/urls.py
+++ example_project/posts/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import PostListView, PostDetailView
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'(?P<pk>\d+)/$', PostDetailView.as_view(), name='detail'),
     url(r'$', PostListView.as_view(), name='list'),
-)
+]
