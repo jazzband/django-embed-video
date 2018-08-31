2d02f8a5d50937e5c394b59f86705ed0777255b4
Jules deMalt <marcosguedes@users.noreply.github.com>
2018-08-31 23:11:06 +0100

    fixed tests to support Vimeo/Youtube HTTPS requests only

--- embed_video/tests/backends/tests_vimeo.py
+++ embed_video/tests/backends/tests_vimeo.py
@@ -8,13 +8,9 @@ from embed_video.backends import VimeoBackend, VideoDoesntExistException
 
 class VimeoBackendTestCase(BackendTestMixin, TestCase):
     urls = (
-        ('http://vimeo.com/72304002', '72304002'),
         ('https://vimeo.com/72304002', '72304002'),
-        ('http://www.vimeo.com/72304002', '72304002'),
         ('https://www.vimeo.com/72304002', '72304002'),
-        ('http://player.vimeo.com/video/72304002', '72304002'),
         ('https://player.vimeo.com/video/72304002', '72304002'),
-        ('http://www.vimeo.com/channels/staffpick/72304002', '72304002'),
         ('https://www.vimeo.com/channels/staffpick/72304002', '72304002'),
     )
 
@@ -22,15 +18,15 @@ class VimeoBackendTestCase(BackendTestMixin, TestCase):
 
     def test_vimeo_get_info_exception(self):
         with self.assertRaises(VideoDoesntExistException):
-            backend = VimeoBackend('http://vimeo.com/123')
+            backend = VimeoBackend('https://vimeo.com/123')
             backend.get_info()
 
     def test_get_thumbnail_url(self):
-        backend = VimeoBackend('http://vimeo.com/72304002')
+        backend = VimeoBackend('https://vimeo.com/72304002')
         self.assertEqual(backend.get_thumbnail_url(),
-                         'http://i.vimeocdn.com/video/446150690_640.jpg')
+                         'https://i.vimeocdn.com/video/446150690_640.jpg')
 
     @patch('embed_video.backends.EMBED_VIDEO_TIMEOUT', 0.000001)
     def test_timeout_in_get_info(self):
-        backend = VimeoBackend('http://vimeo.com/72304002')
+        backend = VimeoBackend('https://vimeo.com/72304002')
         self.assertRaises(requests.Timeout, backend.get_info)
