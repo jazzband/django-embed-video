446b873c955c09574e1a3c45a1b52399bb65840d
Jules deMalt <marcosguedes@users.noreply.github.com>
2018-10-03 19:26:41 +0100

    Revert "readded several (updated) Youtube test urls"
    This reverts commit b32f38095cac47cb22aa1c71b9976f6b62cc3664.

--- embed_video/tests/backends/tests_youtube.py
+++ embed_video/tests/backends/tests_youtube.py
@@ -6,21 +6,16 @@ from embed_video.backends import YoutubeBackend, UnknownIdException
 
 class YoutubeBackendTestCase(BackendTestMixin, TestCase):
     urls = (
-        ('https://youtu.be/jsrRJyHBvzw', 'jsrRJyHBvzw'),
-        ('https://youtu.be/n17B_uFF4cA', 'n17B_uFF4cA'),
         ('https://youtu.be/t-ZRX8984sc', 't-ZRX8984sc'),
         ('https://youtube.com/watch?v=jsrRJyHBvzw', 'jsrRJyHBvzw'),
         ('https://www.youtube.com/v/0zM3nApSvMg?rel=0', '0zM3nApSvMg'),
         ('https://www.youtube.com/embed/0zM3nApSvMg?rel=0', '0zM3nApSvMg'),
-        ('https://www.youtube.com/watch?v=jsrRJyHBvzw', 'jsrRJyHBvzw'),
         ('https://www.youtube.com/watch?v=t-ZRX8984sc', 't-ZRX8984sc'),
         ('https://www.youtube.com/watch?v=iwGFalTRHDA&feature=related', 'iwGFalTRHDA'),
         ('https://www.youtube.com/watch?feature=player_embedded&v=2NpZbaAIXag', '2NpZbaAIXag'),
         ('https://www.youtube.com/watch?v=XPk521voaOE&feature=youtube_gdata_player', 'XPk521voaOE'),
-        ('https://www.youtube.com/watch?v=6xu00J3-g2s&list=PLb5n6wzDlPakFKvJ69rJ9AJW24Aaaki2z', '6xu00J3-g2s'),
         ('https://m.youtube.com/#/watch?v=IAooXLAPoBQ', 'IAooXLAPoBQ'),
-        ('https://m.youtube.com/watch?v=IAooXLAPoBQ', 'IAooXLAPoBQ'),
-        ('https://www.youtube.com/edit?video_id=eBea01qmnOE', 'eBea01qmnOE')
+        ('https://m.youtube.com/watch?v=IAooXLAPoBQ', 'IAooXLAPoBQ')
     )
 
     instance = YoutubeBackend
