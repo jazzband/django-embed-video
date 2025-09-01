6defeacda6dbfda46f21ce34d3d26b04aa5b0d59
Andrey Ilyin <aa.ilin@samolet.ru>
2025-09-01 23:02:53 +0400

    remove python 3.7 support: not available at all

--- embed_video/__init__.py
+++ embed_video/__init__.py
@@ -1,8 +1,5 @@
 import sys
 
-if sys.version_info >= (3, 8):
-    import importlib.metadata as metadata
-else:
-    import importlib_metadata as metadata
+from importlib import metadata
 
 __version__ = metadata.version("django-embed-video")
