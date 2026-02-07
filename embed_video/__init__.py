c3a135b0df9aa432587929870a6643662ebea2de
Mahdi <mahdi.rahimi95@gmail.com>
2026-02-07 13:59:23 +0330

    feat: Add Django 5.2 support and update tooling
    Add Django 5.2, update black/isort, fix Python 3.7 compatibility

--- embed_video/__init__.py
+++ embed_video/__init__.py
@@ -1,3 +1,7 @@
-from importlib.metadata import version
+try:
+    from importlib.metadata import version
+except ImportError:
+    # Python < 3.8
+    from importlib_metadata import version
 
 __version__ = version("django-embed-video")
